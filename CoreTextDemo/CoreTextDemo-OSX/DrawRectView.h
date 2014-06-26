9971ece1ab3455a436a2363777fb5478c2611b20
Yonghui Chen <chyhfj@gmail.com>
2014-06-26 22:57:30 +0800

    create mac workspace

---
+++ CoreTextDemo/CoreTextDemo-OSX/DrawRectView.h
@@ -0,0 +1,13 @@
+//
+//  DrawRectView.h
+//  CoreTextDemo
+//
+//  Created by Chen Yonghui on 6/25/14.
+//  Copyright (c) 2014 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DrawRectView : NSView
+
+@end
---
+++ CoreTextDemo/CoreTextDemo-OSX/TestCaseWindowController.h
@@ -0,0 +1,13 @@
+//
+//  TestCaseWindowController.h
+//  CoreTextDemo
+//
+//  Created by Chen Yonghui on 6/25/14.
+//  Copyright (c) 2014 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TestCaseWindowController : NSWindowController
+@property (nonatomic, strong) Class viewClass;
+@end
